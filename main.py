ca81b1a70c1c295fe431f8de970f8ae3b9cf8ed4
Olof Karlsson <olof.karlsson@getdreams.com>
2019-03-06 20:12:41 +0100

    estimating theta

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 from robotty.wheel_encoder_push import WheelEncoder
 import time
+from threading import Lock
+
 
 def main():
+    h = 0.01  # based on at most 0.5 m/s with 20 ticks per rotation on the wheels :)
+    alpha = 0.9
+    state_lock = Lock()
+
+    x = np.matrix('0;0')
+    P = 1 * np.eye(x.ndim)
+    A = np.matrix([[1, h], [0, alpha]])
+    Q = np.matrix([[0.0001, 0.0001], [0.0001, 1]])
+
+    H = np.matrix([0 1])
+
     def cb(omega):
-        print(omega)
+        state_lock.aquire()
+        R = 0.1 * omega
+        x, P, i, Pi, K = kalman.update(x,omega,P,H,R)
+        state_lock.release()
+        # print("%.2f, %.2f, %.2f".format(i, Pi, K))
+
 
     we = WheelEncoder(cb)
-    time.sleep(1000)
+
+    while True:
+        tic = time.time()
+
+        state_lock.aquire()
+        x, P = predict(x, P, A, Q)
+        state_lock.release()
+        theta = x[0, 0]
+        print(theta)
+
+        toc = time.time()
+        remaining = h - (toc - tic)
+        if (remaining > 0):
+            time.sleep(remaining)
+        else:
+            print("Warning! Update took %.2f s too long!".format(-remaining))
+
 
 if __name__ == "__main__":
     main()
--- robotty/wheel_encoder_push.py
+++ robotty/wheel_encoder_push.py
@@ -2,18 +2,21 @@ import time
 import math
 import RPi.GPIO as GPIO
 
+TICKS_PER_ROT = 20
+
 class WheelEncoder(object):
     def __init__(self, omega_cb):
         # TODO: move this to some global init
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(17, GPIO.IN)
 
-        self.last_t = time()
-        self.omega_cb
-        GPIO.add_event_detect(17, GPIO.FALLING, callback=self.tick, bouncetime=300)  # TODO: tweak the bouncetime!!
+        self.last_t = time.time()
+        self.omega_cb = omega_cb
+        GPIO.add_event_detect(17, GPIO.FALLING, callback=self.tick, bouncetime=10)
 
     def tick(self, pin):
         now = time.time()
         dt = now - self.last_t
-        omega = (2 * math.pi) / (self.ticks_per_rot * dt)
+        omega = (2 * math.pi) / (TICKS_PER_ROT * dt)
+	self.last_t = now
         self.omega_cb(omega)
