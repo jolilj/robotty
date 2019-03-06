2c526cd54836c827f8b527fbbc62f7f2fe2e04ee
Olof Karlsson <olof.karlsson@getdreams.com>
2019-03-06 20:44:45 +0100

    measure theta instead

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-from robotty.wheel_encoder_push import WheelEncoder
+from robotty.wheel_theta_encoder_push import WheelEncoder
 import time
 import numpy as np
 from threading import Lock
 import kalman
 
 h = 0.01  # based on at most 0.5 m/s with 20 ticks per rotation on the wheels :)
-alpha = 0.9
+alpha = 0
 x = np.matrix('0;0')
 P = 1 * np.eye(x.ndim)
 A = np.matrix([[1, h], [0, alpha]])
 Q = np.matrix([[0.0001, 0.0001], [0.0001, 1]])
-H = np.matrix([0, 1])
+H = np.matrix([1, 0])
 
 def main():
     global x, P
     state_lock = Lock()
 
-    def cb(omega):
+    def cb(theta):
         global x, P, i, Pi, K
         state_lock.acquire()
-        R = 0.1 * omega
-        x, P, i, Pi, K = kalman.update(x,omega,P,H,R)
+        R = 0.01
+        x, P, i, Pi, K = kalman.update(x, omega, P, H, R)
         state_lock.release()
         # print("%.2f, %.2f, %.2f".format(i, Pi, K))
 
-
-    we = WheelEncoder(cb)
+    we = WheelEncoder(x[0, 0], cb)
 
     i = 0
     while True:
---
+++ robotty/wheel_theta_encoder_push.py
@@ -0,0 +1,19 @@
+import time
+import math
+import RPi.GPIO as GPIO
+
+TICKS_PER_ROT = 20
+
+class WheelEncoder(object):
+    def __init__(self, theta0, theta_cb):
+        # TODO: move this to some global init
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(17, GPIO.IN)
+
+        self.theta = theta0
+        self.theta_cb = theta_cb
+        GPIO.add_event_detect(17, GPIO.FALLING, callback=self.tick, bouncetime=10)
+
+    def tick(self, pin):
+        self.theta = self.theta + (2 * math.pi / TICKS_PER_ROT)
+        self.theta_cb(self.theta)
