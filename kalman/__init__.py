9b681740c5fdeeb21c216d7c5a5d94838988dc1d
jolilj <lilja.joakim@gmail.com>
2019-03-20 21:19:02 +0100

    add kalman to simulation (NOT WORKING)

--- kalman/__init__.py
+++ kalman/__init__.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 def predict(x,P,A,Q):
-    xp = A*x
-    Pp = A * P * A.T + Q
+    xp = A.dot(x)
+    Pp = A.dot(P.dot(A.T)) + Q
     return xp, Pp
 
 def update(x,z,P,H,R):
     I = np.matrix(np.eye(x.ndim))
-    i = z - H*x
-    Pi = H*P*H.T+R
-    K = P*H.T*np.linalg.inv(Pi)
-    xp = x+K*i
-    Pp = (I-K*H)*P*(I-K*H).T + K*R*K.T
+    i = z - H.dot(x)
+    Pi = H.dot(P.dot(H.T))+R
+    K = P.dot(H.T)/Pi # assume scalar
+    xp = x+K.dot(i)
+    Pp = (I-K.dot(H)).dot(P.dot((I-K.dot(H.T)))) + (K*R).dot(K.T)
     return xp,Pp,i,Pi,K
