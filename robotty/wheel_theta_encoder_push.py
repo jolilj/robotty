87df14281e9c118fc5a4a4d4036e4cf302c6d083
Olof Karlsson <olof.karlsson@getdreams.com>
2019-04-15 19:33:20 +0200

    added missing encoder

--- robotty/wheel_theta_encoder_push.py
+++ robotty/wheel_theta_encoder_push.py
@@ -5,15 +5,15 @@ import RPi.GPIO as GPIO
 TICKS_PER_ROT = 40
 
 class WheelEncoder(object):
-    def __init__(self, theta_cb):
+    def __init__(self, theta_cb, pin):
         # TODO: move this to some global init
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(17, GPIO.IN)
+        GPIO.setup(pin, GPIO.IN)
 
         # self.theta = theta0
         self.last_t = time.time()
         self.theta_cb = theta_cb
-        GPIO.add_event_detect(17, GPIO.FALLING, callback=self.tick, bouncetime=100)
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.tick, bouncetime=10)
 
     def tick(self, pin):
         now = time.time()
