36b67109b31447fbd77bb8472986c9b516d9dc31
Olof Karlsson <olof.karlsson@getdreams.com>
2019-04-15 19:35:50 +0200

    small fix

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ def main():
         state_lock.acquire()
         #if wheel_left.is_still()
         #    kalman.update()
-        A = get_prediction_model(x,h)
-        x, P = kalman.predict(x, P, A, Q)
+        x,A = get_prediction_model(x,h)
+        _, P = kalman.predict(x, P, A, Q)
         state_lock.release()
 
         toc = time.time()
